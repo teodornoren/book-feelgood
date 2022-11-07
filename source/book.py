3002736b53c26978ff2098d91f0bc13ae4d90fc2
eddeT <teodornoren@gmail.com>
2022-11-07 22:14:15 +0100

    linted flake8

--- source/book.py
+++ source/book.py
@@ -3,12 +3,16 @@ import datetime
 import yaml
 import argparse
 
-#how to book and unbook
 """
-https://feelgood.wondr.se/w_booking/activities/cancel/63603a59-215c-46dc-bd89-0cd50a100525/1?force=1  <--- unbook
-https://feelgood.wondr.se/w_booking/activities/participate/{activity_code}}/?force=1 <--- book
+how to unbook:
+https://feelgood.wondr.se/w_booking\
+    /activities/cancel/{activity_code}/1?force=1
+how to book:
+https://feelgood.wondr.se/w_booking\
+    /activities/participate/{activity_code}}/?force=1
 
 """
+
 
 def read_yaml(filename):
     """
@@ -28,9 +32,10 @@ def read_yaml(filename):
     except Exception as expt:
         raise expt
 
+
 def initialize_parser() -> dict:
     """
-    Needed input arguments for this program 
+    Needed input arguments for this program
 
     Returns:
         Dictionary containing parmeters
@@ -40,20 +45,20 @@ def initialize_parser() -> dict:
             time(str)
     """
     parser = argparse.ArgumentParser()
-    
+
     parser.add_argument(
         "-usr", "--username",
         help="The username for feelgood login",
         required=True
     )
     parser.add_argument(
-        "-pw","--password",
+        "-pw", "--password",
         help="The password for feelgood login",
         required=True
         )
 
     parser.add_argument(
-        "-tst","--test",
+        "-tst", "--test",
         nargs='?',
         help="Do a dry run",
         type=bool,
@@ -69,22 +74,22 @@ def initialize_parser() -> dict:
 
     parsed = parser.parse_args()
     input_vars = {
-        "username":parsed.username,
-        "password":parsed.password,
-        "test":parsed.test
+        "username": parsed.username,
+        "password": parsed.password,
+        "test": parsed.test
     }
-    
+
     if parsed.time:
         input_vars["time"] = parsed.time
 
-    
-
     return input_vars
+
 
 def parse_day(day):
     pass
 
-def get_date(offset: int, verbose = False):
+
+def get_date(offset: int, verbose=False):
     dt = datetime.date.today()
     new_date = dt - datetime.timedelta(days=-offset)
 
@@ -98,8 +103,9 @@ def get_date(offset: int, verbose = False):
 
     return new_date
 
+
 def main():
-    #get parser args
+    """get parser args"""
 
     banner = read_yaml("source/banner.yml")
 
@@ -109,21 +115,25 @@ def main():
 
     input_vars = initialize_parser()
 
-    if(input_vars["test"]):
+    if (input_vars["test"]):
         print("---running as test, no booking will be made---")
 
-    if("time" in input_vars):
-        print(f"An input time of {input_vars['time']} overrides the config time of: {config['activity']['time']}")
+    if ("time" in input_vars):
+        print(f"\
+            An input time of {input_vars['time']} overrides \
+                the config time of: {config['activity']['time']}")
         config["activity"]["time"] = input_vars["time"]
 
     date_next_week = get_date(offset=6)
 
-    specific_url = f"https://feelgood.wondr.se/w_booking/activities/list?from={date_next_week}&to={date_next_week}&today=0&location=&user=&mine=0&type=&only_try_it=0&facility=60a7ac3f-b774-4228-a9a3-056c0a10010d"
+    specific_url = f"https://feelgood.wondr.se/w_booking/activities/list?from=\
+        {date_next_week}&to={date_next_week}&today=0&location=&user=&mine=0&\
+            type=&only_try_it=0&facility=60a7ac3f-b774-4228-a9a3-056c0a10010d"
 
     payload = {
         "User": {
-            "email" : input_vars["username"],
-            "password" : input_vars["password"]
+            "email": input_vars["username"],
+            "password": input_vars["password"]
             }
         }
 
@@ -137,32 +147,35 @@ def main():
         r = s.get(config["url"]["home"])
         r = s.get(config["url"]["book"])
 
-        #The magic
         r = s.get(specific_url, headers=headers)
 
         activity_dict = r.json()
         foundSomething = False
         for act in activity_dict["activities"]:
             # Create config file for activity type and time
-            
-            if config["activity"]["name"] in act["ActivityType"]["name"] and config["activity"]["time"] in act["Activity"]["start"]:
-                print(f"Found activity matching: {config['activity']['name']} at time: {config['activity']['time']}")
-                print (act["ActivityType"]["name"])
-                print (act["Activity"]["start"])
-                print (act["Activity"]["id"])
-                booking_url = f"https://feelgood.wondr.se/w_booking/activities/participate/{act['Activity']['id']}/?force=1"
+
+            if config["activity"]["name"] in act["ActivityType"]["name"]\
+                and\
+                    config["activity"]["time"] in act["Activity"]["start"]:
+                print(f"Found activity matching: \
+                    {config['activity']['name']} \
+                        at time: {config['activity']['time']}")
+                print(act["ActivityType"]["name"])
+                print(act["Activity"]["id"])
+                print(act["Activity"]["start"])
+                booking_url = f"https://feelgood.wondr.se/w_booking/\
+                    activities/participate/{act['Activity']['id']}/?force=1"
                 if not input_vars["test"] and not foundSomething:
                     s.post(booking_url, headers=headers)
                 else:
                     print("Booking url that would be used:")
                     print(booking_url)
-                
+
                 foundSomething = True
 
     if not foundSomething:
         print("No matching activity was found, sorry about that.")
 
-            
 
 if __name__ == "__main__":
     main()
