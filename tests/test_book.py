96d755b2f07be491e00f2cd7ef73482e7f2c56bf
eddeT <teodornoren@gmail.com>
2024-03-01 23:04:10 +0100

    coveregegege

--- tests/test_book.py
+++ tests/test_book.py
@@ -4,7 +4,12 @@ from book_feelgood.book import Feelgood_Activity
 
 @pytest.fixture
 def fa_fixture():
-    fa = Feelgood_Activity(url="haha.se", name="Badminton", start="16:00")
+    fa = Feelgood_Activity(
+        url="haha.se",
+        name="Badminton",
+        start="16:00",
+        start_time=123123123123123123,
+    )
     return fa
 
 
@@ -12,3 +17,4 @@ def test_feelgood_activity_init(fa_fixture):
     assert fa_fixture.url == "haha.se"
     assert fa_fixture.name == "Badminton"
     assert fa_fixture.start == "16:00"
+    assert fa_fixture.start_time == 123123123123123123
